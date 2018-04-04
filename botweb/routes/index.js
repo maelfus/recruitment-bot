4e513f496b8ae21c9a0ed3c3b1babaa760e18934
maelfus <aj.tappert@gmail.com>
2018-04-03 23:23:31 -0700

    update title handling in master.dust cleaned up unessesary routes

--- botweb/routes/index.js
+++ botweb/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+/* GET Listings page. */
+router.get('/list', function(req, res, next) {
+  res.render('list', { title: 'Listings' });
 });
 
 module.exports = router;
