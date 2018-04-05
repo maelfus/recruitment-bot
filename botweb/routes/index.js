beec5367a95c9c1d8b12acff4ab164b7b96d8026
maelfus <aj.tappert@gmail.com>
2018-04-04 23:37:39 -0700

    DB INSERT WORKS! GREAT SUCCESS!

--- botweb/routes/index.js
+++ botweb/routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
@@ -10,29 +9,29 @@ router.get('/', function(req, res, next) {
 
 /* Handle POST request from listings page */
 
-router.post('/addguild', function(req, res, next) {
+router.post('/addguild', function(req, res) {
   // Set DB
   var db = req.db;
 
   // Gather FORM variables
-/* Commenting until completion
-  var guildname = res.body.guildname;
-  var contactbnet = res.body.contactbnet;
-  var contactdiscord = res.body.contactdiscord;
-  var region = res.body.region;
-  var server = res.body.server;
-  var faction = res.body.faction;
-  var classes = res.body.classes; // array
-  var language = res.body.language;
-  var raidtype = res.body.raidtype;
-  // var raidtimes = res.body.raidtimes;
-  // var progress = res.body.progress;
-  var discordlink = res.body.discordlink;
-  var website = res.body.website;
-  var description = res.body.description;
-  // var user = res.body.user; // need to pull oauth user info
-  var lastupdate =  new Date();
-*/
+/* Commenting until completion */
+  var guildname = req.body.guildname;
+  var contactbnet = req.body.contactbnet;
+  var contactdiscord = req.body.contactdiscord;
+  var region = req.body.region;
+  var server = req.body.server;
+  var faction = req.body.faction;
+  var classes = req.body.classes; // array
+  var language = req.body.language;
+  var raidtype = req.body.raidtype;
+  // var raidtimes = req.body.raidtimes;
+  // var progress = req.body.progress;
+  var discordlink = req.body.discordlink;
+  var website = req.body.website;
+  var description = req.body.description;
+  // var user = req.body.user; // need to pull oauth user info
+  var lastupdated =  new Date();
+/* */
 
   // Set Collection
   var collection = db.get('listingcollection');
@@ -61,7 +60,7 @@ router.post('/addguild', function(req, res, next) {
     }
     else {
       // Forward back to /list
-      res.redirect("/list", { message: 'Successfully added: ' + guildname })
+      res.render('list', { title: 'Listings', message: 'Successfully added new listing' });
     }
   });
 });
