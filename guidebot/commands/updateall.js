0fb288efccd38bf063b8661aa82373636916c7fd
Adam Tappert <aj.tappert@gmail.com>
2018-04-19 02:41:21 -0700

    Basic Class filtering added, with example config

--- guidebot/commands/updateall.js
+++ guidebot/commands/updateall.js
@@ -5,50 +5,59 @@ var db = monk('localhost:27017/botweb');
 var collection = db.get("listingcollection");
 
 
-
 exports.run = (client, message, args, level) => { //eslint-disable-line no-unused-vars
   message.channel.bulkDelete(100, true)
     .then(messages => console.log(`Deleted ${messages.size} messages.`) )
     .catch((err) => {console.log(err)});
 
-  collection.find({}).each((guild, {close, pause, resume}) => {
-    var f = "="; //filler character for guildname header outline
+  // Get Server Settings
+  var classes = message.settings;
 
-    message.channel.send(`
-====${f.repeat(guild.guildname.length)}====
-    ${guild.guildname}
-====${f.repeat(guild.guildname.length)}====
+  collection.find({}).each((guild, {close, pause, resume}) => {
+    // Filtering and Formatting for Classes
+    var classList = '';
+    classList += classes.recruitDeathKnight == "true" ? `\n  • Death Knight : ${guild.deathknight.join(", ")}` : ``;
+    classList += classes.recruitDemonHunter == "true" ? `\n  • Demon Hunter : ${guild.demonhunter.join(", ")}` : ``;
+    classList += classes.recruitDruid == "true"       ? `\n  • Druid   : ${guild.druid.join(", ")}` : ``;
+    classList += classes.recruitHunter == "true"      ? `\n  • Hunter  : ${guild.hunter.join(", ")}` : ``;
+    classList += classes.recruitMage == "true"        ? `\n  • Mage    : ${guild.mage.join(", ")}` : ``;
+    classList += classes.recruitMonk == "true"        ? `\n  • Monk    : ${guild.monk.join(", ")}` : ``;
+    classList += classes.recruitPaladin == "true"     ? `\n  • Paladin : ${guild.paladin.join(", ")}` : ``;
+    classList += classes.recruitPriest == "true"      ? `\n  • Priest  : ${guild.priest.join(", ")}` : ``;
+    classList += classes.recruitRogue == "true"       ? `\n  • Rogue   : ${guild.rogue.join(", ")}` : ``;
+    classList += classes.recruitShaman == "true"      ? `\n  • Shaman  : ${guild.shaman.join(", ")}` : ``;
+    classList += classes.recruitWarlock == "true"     ? `\n  • Warlock : ${guild.warlock.join(", ")}` : ``;
+    classList += classes.recruitWarrior == "true"     ? `\n  • Warrior : ${guild.warrior.join(", ")}` : ``;
+
+
+    message.channel.send(`= ${guild.guildname} =
+
 Contacts  ::
-    Discord    : ${guild.contactdiscord}
-    Battle.net : ${guild.contactbnet}
+  • Discord    : ${guild.contactdiscord}
+  • Battle.net : ${guild.contactbnet}
 Region    :: ${guild.region}
 Server    :: ${guild.server}
 Faction   :: ${guild.faction}
-Classes   ::
-  • Death Knight : ${guild.deathknight}
-  • Demon Hunter : ${guild.demonhunter}
-  • Druid   : ${guild.druid}
-  • Hunter  : ${guild.hunter}
-  • Mage    : ${guild.mage}
-  • Monk    : ${guild.monk}
-  • Paladin : ${guild.paladin}
-  • Priest  : ${guild.priest}
-  • Rogue   : ${guild.rogue}
-  • Shaman  : ${guild.shaman}
-  • Warlock : ${guild.warlock}
-  • Warrior : ${guild.warrior}
+Classes   :: ${classList}
 Language  :: ${guild.language}
 Raid Type :: ${guild.raidtype}
 Progress  :: (NYI)
 Raid Times:: (NYI)
+  •
+  •
+  •
+  •
+  •
+  •
+  •
 Discord   :: ${guild.discordlink}
 Website   :: ${guild.website}
 Description ::
 ${guild.description}
 ID        ::  ${guild._id}`, {code: "asciidoc"});
-}).then(() => {
-  message.channel.send(`To have your guild listed, visit http://127.0.0.1:3000/`);
-}).catch((err) => { console.log(err) });
+  }).then(() => {
+    message.channel.send(`To have your guild listed, visit http://127.0.0.1:3000/`);
+  }).catch((err) => { console.log(err) });
 };
 
 exports.conf = {
