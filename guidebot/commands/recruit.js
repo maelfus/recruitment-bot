6c25372958f48fab39d60cb288d765f8ec1d7f07
Adam Tappert <aj.tappert@gmail.com>
2018-04-24 15:36:10 -0700

    recruit.js: dont overwrite channel with message.channel.id throw error in settings dont exist.

--- guidebot/commands/recruit.js
+++ guidebot/commands/recruit.js
@@ -5,39 +5,48 @@ const db = monk('localhost:27017/botweb');
 const settings = db.get("serversettings");
 
 
-exports.run = (client, message, args, level) => {
+exports.run = async (client, message, args, level) => {
+  try {
+    const oldSettings = await settings.findOne( { "serverid" : message.guild.id } );
+    if (oldSettings == null) {
+      throw "No settings available for this server. Use \`init\` to configure.";
+    } else {
+
   // Reset classes so that the update only includes the classes in the command
-  let classes = {
-    deathknight : false,
-    demonhunter : false,
-    druid : false,
-    hunter : false,
-    mage : false,
-    monk : false,
-    paladin : false,
-    priest : false,
-    rogue : false,
-    shaman : false,
-    warlock : false,
-    warrior : false
+    let classes = {
+      deathknight : false,
+      demonhunter : false,
+      druid : false,
+      hunter : false,
+      mage : false,
+      monk : false,
+      paladin : false,
+      priest : false,
+      rogue : false,
+      shaman : false,
+      warlock : false,
+      warrior : false
+    }
+
+    // set our args to the classes obj (ignore the 'random' property)
+    for ( i in args ) {
+      classes.hasOwnProperty(args[i]) ? classes[args[i]] = true : typeof args[i] !== 'function' ? await message.channel.send(`Invalid class, ${args[i]}, skipping...`) : null;
+    }
+
+    settings.findOneAndUpdate( { "serverid" : message.guild.id }, { "serverid" : message.guild.id, "classes" : classes, "channel": oldSettings.channel })
+      .then(() => {
+        let reportClasses = '';
+        for ( i in classes ) {
+          classes[i] === true ? reportClasses += `${i} `: null;
+        }
+        message.channel.send(`Updated class settings! Filter set to: ${reportClasses}`);
+      });
+    }
+  } catch (e) {
+    message.channel.send(e);
   }
-
-  // set our args to the classes obj (ignore the 'random' property)
-  for ( i in args ) {
-    console.log(args[i]);
-    classes.hasOwnProperty(args[i]) ? classes[args[i]] = true : typeof args[i] !== 'function' ? message.channel.send(`Invalid class, ${args[i]}, skipping...`) : null;
-  }
-
-  settings.findOneAndUpdate( { "serverid" : message.guild.id }, { "serverid" : message.guild.id, "classes" : classes, "channel": message.channel.id })
-    .then(() => {
-      let reportClasses = '';
-      for ( i in classes ) {
-        classes[i] === true ? reportClasses += `${i} `: null;
-      }
-      message.channel.send(`Updated class settings! Filter set to: ${reportClasses}`);
-    });
-
 };
+
 exports.conf = {
   enabled: true,
   guildOnly: false,
