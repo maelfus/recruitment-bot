919308bf72d157bd30ad6d19f6ad3c723be858fb
Adam Tappert <aj.tappert@gmail.com>
2018-04-25 16:00:57 -0700

    recruit.js/init.js: fixed case sensitivity for 'classes'

--- guidebot/commands/recruit.js
+++ guidebot/commands/recruit.js
@@ -12,35 +12,34 @@ exports.run = async (client, message, args, level) => {
       throw "No settings available for this server. Use \`init\` to configure.";
     } else {
 
-  // Reset classes so that the update only includes the classes in the command
-    let classes = {
-      deathknight : false,
-      demonhunter : false,
-      druid : false,
-      hunter : false,
-      mage : false,
-      monk : false,
-      paladin : false,
-      priest : false,
-      rogue : false,
-      shaman : false,
-      warlock : false,
-      warrior : false
-    }
+      // Reset classes so that the update only includes the classes in the command
+      let classes = {
+        deathknight : false,
+        demonhunter : false,
+        druid : false,
+        hunter : false,
+        mage : false,
+        monk : false,
+        paladin : false,
+        priest : false,
+        rogue : false,
+        shaman : false,
+        warlock : false,
+        warrior : false
+      }
 
-    // set our args to the classes obj (ignore the 'random' property)
-    for ( i in args ) {
-      classes.hasOwnProperty(args[i]) ? classes[args[i]] = true : typeof args[i] !== 'function' ? await message.channel.send(`Invalid class, ${args[i]}, skipping...`) : null;
-    }
-
-    settings.findOneAndUpdate( { "serverid" : message.guild.id }, { "serverid" : message.guild.id, "classes" : classes, "channel": oldSettings.channel })
-      .then(() => {
-        let reportClasses = '';
-        for ( i in classes ) {
-          classes[i] === true ? reportClasses += `${i} `: null;
-        }
-        message.channel.send(`Updated class settings! Filter set to: ${reportClasses}`);
-      });
+      // set our args to the classes obj (ignore the 'random' property)
+      for ( i in args ) {
+        typeof args[i] === 'function' ? null : classes.hasOwnProperty(args[i].toLowerCase()) ? classes[args[i].toLowerCase()] = true : await message.channel.send(`Invalid class, ${args[i]}, skipping...`);
+        //classes.hasOwnProperty(args[i].toLowerCase()) ? classes[args[i].toLowerCase()] = true : typeof args[i] !== 'function' ? await message.channel.send(`Invalid class, ${args[i]}, skipping...`) : null;
+      }
+      // Update server settings for the new class filter and report to channel
+      await settings.findOneAndUpdate( { "serverid" : message.guild.id }, { "serverid" : message.guild.id, "classes" : classes, "channel": oldSettings.channel });
+      let reportClasses = '';
+      for ( i in classes ) {
+        classes[i] === true ? reportClasses += `${i} `: null;
+      }
+      await message.channel.send(`Updated class settings! Filter set to: ${reportClasses}`);
     }
   } catch (e) {
     message.channel.send(e);
